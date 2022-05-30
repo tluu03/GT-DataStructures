5b433e0de43cba02a76da4b41b8c4666804e03b8
tluu03 <17326017+tluu03@users.noreply.github.com>
2022-05-30 17:19:20 -0400

    switch case added

--- Calculator.java
+++ Calculator.java
@@ -1,6 +1,27 @@
 import java.util.*;
 public class Calculator {
     public static void main(String[] args){
-        while
+        int add, subtract;
+        double multiply, divide;
+        String word;
+        Scanner input = new Scanner(System.in);
+        boolean cont = true;
+
+        while(cont){
+            System.out.println("List of operations: add subtract multiply divide alphabetize");
+            System.out.println("Enter an operation:");
+            String operation = input.nextLine();
+            switch(operation.toLowerCase()){
+                case "add":
+                case "subtract":
+                case "multiply":
+                case "divide":
+                case "alphabetize":
+                default:
+                System.out.println("Invalid input entered. Terminating...");
+                cont = false;
+            }
+        }
+        
     }
 }
