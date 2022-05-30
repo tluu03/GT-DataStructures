1e69ef94c772741091e24f19c64b74d088ef6824
tluu03 <17326017+tluu03@users.noreply.github.com>
2022-05-30 18:13:46 -0400

    finished add

--- Calculator.java
+++ Calculator.java
@@ -1,4 +1,4 @@
-import java.util.*;
+import java.util.Scanner;
 public class Calculator {
     public static void main(String[] args){
         int add, subtract;
@@ -12,14 +12,29 @@ public class Calculator {
             System.out.println("Enter an operation:");
             String operation = input.nextLine();
             switch(operation.toLowerCase()){
-                case "add":
+                case "add":{
+                    Scanner scan = new Scanner(System.in);
+                    System.out.println("Enter two integers:");
+                    String input1 = scan.nextLine();
+                    String[] input2 = input1.split(" ");
+                    try{
+                    int x = Integer.valueOf(input2[0]);
+                    int y = Integer.valueOf(input2[1]);
+                    int z = x + y;
+                    System.out.println("Answer: " + z);
+                    break;
+                    }catch(NumberFormatException e){
+                        cont = false;
+                    }
+                }
                 case "subtract":
                 case "multiply":
                 case "divide":
                 case "alphabetize":
-                default:
+                default:{
                 System.out.println("Invalid input entered. Terminating...");
                 cont = false;
+                }
             }
         }
         
