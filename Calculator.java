6cfeabca893eba8893b4ff977c80b089dc11ce99
tluu03 <17326017+tluu03@users.noreply.github.com>
2022-06-01 01:31:00 -0400

    implemented alphabetize, works here, not on GS

--- Calculator.java
+++ Calculator.java
@@ -11,7 +11,7 @@ public class Calculator {
             String operation = input.nextLine();
             switch(operation.toLowerCase()){
                 case "add":{
-                    //Scanner input = new Scanner(System.in);
+                    System.out.println();
                     System.out.println("Enter two integers:");
                     String input1 = input.nextLine();
                     String[] input2 = input1.split(" ");
@@ -20,12 +20,14 @@ public class Calculator {
                     int y = Integer.valueOf(input2[1]);
                     int z = x + y;
                     System.out.println("Answer: " + z);
+                    System.out.println();
                     break;
                     }catch(NumberFormatException e){
                         cont = false;
                     }
                 }
                 case "subtract":
+                    System.out.println();
                     System.out.println("Enter two integers:");
                     String input1 = input.nextLine();
                     String[] input2 = input1.split(" ");
@@ -34,11 +36,13 @@ public class Calculator {
                     int y = Integer.valueOf(input2[1]);
                     int z = x + y;
                     System.out.println("Answer: " + z);
+                    System.out.println();
                     break;
                     }catch(NumberFormatException e){
                         cont = false;
                     }
                 case "multiply":
+                    System.out.println();
                     System.out.println("Enter two integers:");
                     String inputx = input.nextLine();
                     String[] inputy = inputx.split(" ");
@@ -47,11 +51,13 @@ public class Calculator {
                     double y = Double.parseDouble(inputy[1]);
                     double z = x * y;
                     System.out.printf("Answer: %.2f%n", z);
+                    System.out.println();
                     break;
                     }catch(NumberFormatException e){
                         cont = false;
                     }
                 case "divide":
+                    System.out.println();
                     System.out.println("Enter two integers:");
                     String inputz = input.nextLine();
                     String[] inputw = inputz.split(" ");
@@ -61,6 +67,7 @@ public class Calculator {
                         double y = Double.parseDouble(inputw[1]);
                         double z = x / y;
                         System.out.printf("Answer: %.2f%n", z);
+                        System.out.println();
                         break;
                         }catch(NumberFormatException e){
                         cont = false;
@@ -70,12 +77,33 @@ public class Calculator {
                     cont = false;
                 }
                 case "alphabetize":
+                    System.out.println();
+                    System.out.println("Enter two words:");
+                    String inputq = input.nextLine();
+                    String[] inputr = inputq.split(" ");
+                    String t = inputr[0].toLowerCase();
+                    String u = inputr[1].toLowerCase();
+                    int compare = t.compareTo(u);
+                    if (compare > 0){
+                        System.out.println(inputr[1] + " comes before " + inputr[0] + " alphabetically.");
+                        System.out.println();
+                        break;
+                    }
+                    else if(compare < 0){
+                        System.out.println(inputr[0] + " comes before " + inputr[1] + " alphabetically.");
+                        System.out.println();
+                        break;
+                    }
+                    else{
+                        System.out.println("Chicken or Egg.");
+                        System.out.println();
+                        break;
+                    }
                 default:{
-                System.out.println("Invalid input entered. Terminating...");
-                cont = false;
+                    System.out.println("Invalid input entered. Terminating...");
+                    cont = false;
                 }
             }
         }
-        
     }
 }
