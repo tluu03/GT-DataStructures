2a8996146579b9119be8e24eaebe678347d59a3f
tluu03 <17326017+tluu03@users.noreply.github.com>
2022-06-10 16:36:18 -0400

    got errthang done except for one

--- Battleship.java
+++ Battleship.java
@@ -1,14 +1,23 @@
+//I worked on the homework assignment alone, using only course materials.
 import java.util.Scanner;
-
+/**
+ * @author Tony Luu
+ * @version version 1
+ * Battle ship!
+ */
 public class Battleship {
+    /**
+     * main method
+     * @param args args
+     */
     public static void main(String[] args) {
-        boolean win = false;
+        boolean player1Turn = true;
         char[][] player1 = new char[5][5];
         char[][] player2 = new char[5][5];
         int player1Score = 5;
         int player2Score = 5;
         Scanner scan = new Scanner(System.in);
-        System.out.printf("Welcome to Battleship!\n");
+        System.out.printf("\nWelcome to Battleship!\n");
         for (int i = 1; i < 3; i++) {
             System.out.println("PLAYER " + i + ", ENTER YOUR SHIPS' LOCATION.");
             for (int j = 1; j < player1.length + 1; j++) {
@@ -44,96 +53,108 @@ public class Battleship {
                 }
             }
             if (i == 1) {
-                printBattleShip(player1);
-                // for(int z = 0; z < 101; z++){
-                // System.out.println("");
-                // }
+                printBattleShip(player1, 'T');
+                for (int z = 0; z < 101; z++) {
+                    System.out.println("");
+                }
             } else {
-                printBattleShip(player2);
-                // for(int z = 0; z < 101; z++){
-                // System.out.println("");
-                // }
-            }
-        }
-
-        do {
-            for (int i = 1; i < 3; i++) {
-                switch (i) {
-                    case 1:
-                        System.out.println("Player 1, enter hit row/column:");
-                        String input1 = scan.nextLine();
-                        String[] input2 = input1.split(" ");
-                        int x = Integer.valueOf(input2[0]);
-                        int y = Integer.valueOf(input2[1]);
-                        try {
-                            if (player2[x][y] == '@') {
-                                System.out.println("PLAYER 1 HIT PLAYER 2's SHIP!");
-                                player2Score -= 1;
-                            } else if (player2[x][y] == 'O') {
-                                System.out.println("You already fired on this spot. Choose different coordinates");
-                                i = i - 1;
-                            } else {
-                                System.out.println("PLAYER 1 MISSED!");
-                                player2[x][y] = 'O';
-                                // sets player1 temp board to have a O
-                                // print temp board
-                            }
-                        } catch (IndexOutOfBoundsException e) {
-                            System.out.println("Invalid coordinates. Choose different coordinates.");
-                            i = i - 1;
-                        }
-                    case 2:
-                        System.out.println("Player 2, enter hit row/column:");
-                        String input3 = scan.nextLine();
-                        String[] input4 = input3.split(" ");
-                        int z = Integer.valueOf(input4[0]);
-                        int w = Integer.valueOf(input4[1]);
-                        try {
-                            if (player1[z][w] == '@') {
-                                System.out.println("PLAYER 2 HIT PLAYER 1'S SHIP!");
-                                player1Score -= 1;
-                                // print temp board
-                            } else if (player1[z][w] == 'O') {
-                                System.out.println("You already fired on this spot. Choose different coordinates");
-                                i = i - 1;
-                            } else {
-                                // sets player1 temp board to have a O
-                                System.out.println("PLAYER 2 MISSED!");
-                                player1[z][w] = 'O';
-                                // print temp board
-                            }
-                        } catch (IndexOutOfBoundsException e) {
-                            System.out.println("Invalid coordinates. Choose different coordinates.");
-                            i = i - 1;
-                        }
-                    default:
-                        if (player1Score == 0) {
-                            System.out.println("PLAYER 2 WINS! YOU SUNK ALL OF YOUR OPPONENT'S SHIPS!");
-                            win = true;
-                        } else if (player2Score == 0) {
-                            System.out.println("PLAYER 2 WINS! YOU SUNK ALL OF YOUR OPPONENT'S SHIPS!");
-                            win = true;
-                        }
-                        break;
+                printBattleShip(player2, 'T');
+                for (int z = 0; z < 101; z++) {
+                    System.out.println("");
                 }
             }
-        } while (win = false);
+        }
+        // this needs work
+        do {
+            if (player1Turn) {
+                System.out.println("Player 1, enter hit row/column:");
+                String input1 = scan.nextLine();
+                String[] input2 = input1.split(" ");
+                int x = Integer.valueOf(input2[0]);
+                int y = Integer.valueOf(input2[1]);
+                try {
+                    if (player2[x][y] == '@') {
+                        System.out.println("PLAYER 1 HIT PLAYER 2's SHIP!");
+                        player2[x][y] = 'X';
+                        player2Score -= 1;
+                        if (player2Score <= 0) {
+                            System.out.println("PLAYER 1 WINS! YOU SUNK ALL OF YOUR OPPONENT'S SHIPS!");
+                            System.out.println("");
+                            break;
+                        }
+                        printBattleShip(player2, '@');
+                        System.out.println("");
+                        player1Turn = false;
+                    } else if (player2[x][y] == 'O') {
+                        System.out.println("You already fired on this spot. Choose different coordinates");
+                    } else {
+                        System.out.println("PLAYER 1 MISSED!");
+                        player2[x][y] = 'O';
+                        player1Turn = false;
+                        printBattleShip(player2, '@');
+                        System.out.println("");
+                    }
+                } catch (IndexOutOfBoundsException e) {
+                    System.out.println("Invalid coordinates. Choose different coordinates.");
+                }
+            } else {
+                System.out.println("Player 2, enter hit row/column:");
+                String input3 = scan.nextLine();
+                String[] input4 = input3.split(" ");
+                int z = Integer.valueOf(input4[0]);
+                int w = Integer.valueOf(input4[1]);
+                try {
+                    if (player1[z][w] == '@') {
+                        System.out.println("PLAYER 2 HIT PLAYER 1's SHIP!");
+                        player1[z][w] = 'X';
+                        player1Score -= 1;
+                        if (player1Score <= 0) {
+                            System.out.println("PLAYER 2 WINS! YOU SUNK ALL OF YOUR OPPONENT'S SHIPS!");
+                            System.out.println("");
+                            break;
+                        }
+                        printBattleShip(player1, '@');
+                        System.out.println("");
+                        player1Turn = true;
+                    } else if (player1[z][w] == 'O') {
+                        System.out.println("You already fired on this spot. Choose different coordinates");
+                    } else {
+                        // sets player1 temp board to have a O
+                        System.out.println("PLAYER 2 MISSED!");
+                        player1[z][w] = 'O';
+                        printBattleShip(player1, '@');
+                        System.out.println("");
+                        player1Turn = true;
+                    }
+                } catch (IndexOutOfBoundsException e) {
+                    System.out.println("Invalid coordinates. Choose different coordinates.");
 
-        // print final boards
-        // need to figure out the hidden ships thing
+                }
+            }
+            // need to figure out the hidden ships thing
+        } while (player1Score > 0 || player2Score > 0);
+        if (player1Score > 0 || player2Score > 0) {
+            System.out.println("Final boards:");
+            printBattleShip(player1, 'T');
+            System.out.println("");
+            printBattleShip(player2, 'T');
+        }
     }
 
-    private static void printBattleShip(char[][] player) {
+    private static void printBattleShip(char[][] player, char banned) {
         System.out.print("  ");
         for (int row = -1; row < 5; row++) {
             if (row > -1) {
                 System.out.print(row + " ");
             }
             for (int column = 0; column < 5; column++) {
+                char state = row == -1 ? ' ' : player[row][column];
                 if (row == -1) {
                     System.out.print(column + " ");
-                } else {
+                } else if (state != banned && (state == '@' || state == 'O' || state == 'X')) {
                     System.out.print(player[row][column] + " ");
+                } else {
+                    System.out.print("- ");
                 }
             }
             System.out.println("");
